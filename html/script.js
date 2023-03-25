5ef62584827fa1aa6d351d6a93bbd857fcd7a98e
314rs <lars.borntraeger@gmx.de>
2023-03-25 22:35:16 +0100

    rewrite js ttt tree representation in

--- html/script.js
+++ html/script.js
@@ -1,77 +1,18 @@
+/* let socket = new WebSocket("ws://localhost:8765")
 
-class TttLeaf extends HTMLElement 
-{
-    static whosTurn = 1;
-    static start = true;
-    static actives = [];
-
-    get index() {
-        return Array.from(this.parentElement.children).indexOf(this);
-    }
-
-    full() {
-        this.setAttribute("full", "");
-    }
-
-    constructor() 
-    {
-        super();
-        this.addEventListener("click", e => {
-            if (! this.hasAttribute("p") && ! this.parentElement.hasAttribute("p") && ! this.hasAttribute("full") && this.hasAttribute("active") || TttLeaf.start) {
-                this.setStone();
-                for (const node of TttLeaf.actives) {
-                    node.removeAttribute("active");
-                }
-                TttLeaf.actives = [];
-                this.parentElement.checkWinner([this.index]);
-                TttLeaf.start = false;
-            }
-
-        });
-    }
-
-    connectedCallback() {
-        TttLeaf.actives.push(this);
-        this.setAttribute("active", "");
-    }
-
-    setStone() {
-        if (TttLeaf.whosTurn == 1) {
-            this.setAttribute("p","1");
-            TttLeaf.whosTurn = 2;
-        } else {
-            this.setAttribute("p", "2");
-            TttLeaf.whosTurn = 1;
-        }
-        this.setAttribute("full", "");
-    }
-
-    markActive() {
-        if (!this.hasAttribute("full")) {
-            this.setAttribute("active", "");
-            TttLeaf.actives.push(this);
-        }
-    }
-
-    
-}
-customElements.define("ttt-leaf", TttLeaf);
-
-
+socket.onmessage = event => {
+    console.log(event.data);
+} */
 
 class TttNode extends HTMLElement
 {
-    static maxdepth = 0;
+    static patterns = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
 
-    constructor() 
-    {
-        super();
-    }
-
+    
     get index() {
         return Array.from(this.parentElement.children).indexOf(this);
     }
-
+    
     get depth() {
         return parseInt(this.getAttribute("depth"));
     }
@@ -88,30 +29,59 @@ class TttNode extends HTMLElement
         }
     }
 
+    setStone() {
+        if (this.closest("ttt-game").whosTurn == 1) {
+            this.setAttribute("p","1");
+            this.closest("ttt-game").whosTurn = 2;
+        } else {
+            this.setAttribute("p", "2");
+            this.closest("ttt-game").whosTurn = 1;
+        }
+        this.full();
+    }
+    
+    constructor() 
+    {
+        super();
+        
+    }
+    
     connectedCallback() 
     {
-        TttNode.maxdepth = Math.max(this.depth, TttNode.maxdepth);
-
-        if (this.depth == 1) {
-            for (let i = 0; i < 9; i++) {
-                this.appendChild(document.createElement("ttt-leaf"));      
-            }      
-        } else if (this.depth > 1) {
+        if (this.depth < 0) {
+            alert("Error: ttt-node with depth < 0 exists.");
+        } else if (this.depth == 0) {
+            if (this.parentElement.depth == 1) {
+                this.addEventListener("click", e => {
+                    if (!this.hasAttribute("full") && this.hasAttribute("active")) {
+                        this.setStone();
+                        for (const node of TttGame.actives) {
+                            node.removeAttribute("active");
+                        }
+                        TttGame.actives = [];
+                        this.parentElement.checkWinner([this.index]);
+                    }
+                });   
+            }
+            TttGame.actives.push(this);
+            this.setAttribute("active", "");        
+        } else {
             for (let i = 0; i < 9; i++) {
                 var child = new TttNode()
                 child.setAttribute("depth", this.depth-1)
                 this.appendChild(child).cloneNode(true);
             } 
+            this.style.borderWidth = (3 * this.depth) + "px";
         }
-        if (this.depth == TttNode.maxdepth) {
-            this.setAttribute("rootnode", "");
-        }
-
-        this.style.borderWidth = (3 * this.depth) + "px";
     }
 
     checkWinner(pos) {
-              
+        if (this.depth == 0) {
+            alert("Error: checkWinner was called on leaf");
+            return;
+        }
+
+        // create array of state
         let state = Array.from(this.children).map((child) => {
             if (child.hasAttribute("p")) {
                 return parseInt(child.getAttribute("p"));
@@ -119,19 +89,14 @@ class TttNode extends HTMLElement
                 return 0;
             }
         });
-        const patterns = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
 
-        for (const line of patterns) {
+        for (const line of TttNode.patterns) {
             let lineSet = new Set(line.map((index) => state[index]));
             if (lineSet.size == 1 && ! lineSet.has(0)) {
                 this.setAttribute("p", lineSet.values().next().value);
                 this.full();
                 pos.push(this.index); 
-                if (this.depth < TttNode.maxdepth) {
-                    this.parentElement.checkWinner(pos);
-                } else {
-                    console.log("end!")
-                }
+                this.parentElement.checkWinner(pos);
                 return;
             } 
         }
@@ -139,19 +104,11 @@ class TttNode extends HTMLElement
         if (Array.from(this.children).filter(child => child.hasAttribute("full")).length == 9) {
             this.full();
             pos.push(this.index); 
-            if (this.depth < TttNode.maxdepth) {
-                this.parentElement.checkWinner(pos);
-            } else {
-                console.log("end!")
-            }
-           return;
+            this.parentElement.checkWinner(pos);
+            return;
         }
 
-        if (this.depth < TttNode.maxdepth) {
-            this.parentElement.findActive(pos);
-        } else {
-            this.markActive()
-        }
+        this.parentElement.findActive(pos);
     }
 
     findActive(pos) {
@@ -169,19 +126,64 @@ class TttNode extends HTMLElement
 
     markActive() {
         if (!this.hasAttribute("full")) {
-            for (const leaf of this.children) {
-                leaf.markActive();
+            if (this.depth == 0) {
+                this.setAttribute("active", "");
+                TttGame.actives.push(this);
+            } else {
+                for (const leaf of this.children) {
+                    leaf.markActive();
+                }
             }
         } 
     }
 }
 customElements.define("ttt-node", TttNode);
 
+class TttGame extends HTMLElement {
+
+    static actives = [];
+    whosTurn = 1;
+
+
+    set depth(val) {
+        this.setAttribute("depth", val);
+        return val;
+    }
+    get depth() {
+        return parseInt(this.getAttribute("depth"));
+    }
+
+    constructor() 
+    {
+        super();
+    }
+
+    connectedCallback() {
+        if (this.depth > 0) {
+            for (let i = 0; i < 9; i++) {
+                var child = new TttNode()
+                child.setAttribute("depth", this.depth - 1)
+                this.appendChild(child).cloneNode(true);
+            } 
+        }
+    }
+
+    findActive(pos) {
+        const index = pos.pop();
+            if (!this.children[index].hasAttribute("full")) {
+                this.children[index].findActive(pos);
+            } else {
+                this.markActive();
+            }
+    }
+
+
+
+}
+customElements.define("ttt-game", TttGame);
 
 function autoplay() {
-    if (true/* TttLeaf.whosTurn == 1 */) {
-        TttLeaf.actives[Math.floor(Math.random() * (TttLeaf.actives.length))].click();
-    }
+    TttGame.actives[Math.floor(Math.random() * (TttGame.actives.length))].click();
     setTimeout(autoplay, 10);
 }
 
